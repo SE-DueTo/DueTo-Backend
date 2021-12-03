c978a278f1e8e8cd55cde3e35dec6e3cbdaaf63c
Akkjon <akkjon@outlook.de>
2021-12-03 17:25:34 +0100

    added swagger

---
+++ src/main/java/de/dueto/backend/swagger/ApiDocumentationConfig.java
@@ -0,0 +1,27 @@
+package de.dueto.backend.swagger;
+
+import io.swagger.annotations.*;
+
+@SwaggerDefinition(
+        info = @Info(
+                description = "DueTo-Backend API",
+                version = "V12.0.12",
+                title = "DueTo-Backend API",
+                contact = @Contact(
+                        name = "DueTo-Team",
+                        email = "sedueto@gmail.com",
+                        url = "https://www.sedueto.wordpress.com"
+                ),
+                license = @License(
+                        name = "Apache 2.0",
+                        url = "http://www.apache.org/licenses/LICENSE-2.0"
+                )
+        ),
+        consumes = {"application/json", "application/xml"},
+        produces = {"application/json", "application/xml"},
+        schemes = {SwaggerDefinition.Scheme.HTTP, SwaggerDefinition.Scheme.HTTPS},
+        externalDocs = @ExternalDocs(value = "Read This For Sure", url = "http://in28minutes.com")
+)
+public interface ApiDocumentationConfig {
+
+}
