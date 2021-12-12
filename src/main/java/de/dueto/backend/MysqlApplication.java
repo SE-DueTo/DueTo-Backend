19da132bc139b055b1b967028b41e507b22f260e
Akkjon <akkjon@outlook.de>
2021-12-12 16:28:56 +0100

    debugging

--- src/main/java/de/dueto/backend/MysqlApplication.java
+++ src/main/java/de/dueto/backend/MysqlApplication.java
@@ -5,10 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
+import java.util.Map;
+
 @SpringBootApplication
 public class MysqlApplication {
 
     public static void main(String[] args) {
+        for(Map.Entry<String, String> entry : System.getenv().entrySet()) {
+            System.out.println(entry.getKey() + " " + entry.getValue());
+        }
         SpringApplication.run(MysqlApplication.class, args);
     }
 
