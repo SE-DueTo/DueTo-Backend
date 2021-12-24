0dcb706e4e31e41950e08bafee8ce83f2bf87d45
Akkjon <akkjon@outlook.de>
2021-12-24 17:14:09 +0100

    MMEJ-202 fixed AuthorizationMapper secret in code

--- src/main/java/de/dueto/backend/controller/v1/AuthorizationMapper.java
+++ src/main/java/de/dueto/backend/controller/v1/AuthorizationMapper.java
@@ -1,6 +1,7 @@
 package de.dueto.backend.controller.v1;
 
 import de.dueto.backend.model.user.User;
+import de.dueto.backend.security.secret.JwtSecret;
 import de.dueto.backend.service.UserService;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
@@ -9,13 +10,15 @@ import org.springframework.stereotype.Component;
 public class AuthorizationMapper {
 
     private final UserService userService;
+    private final String jwtSecret;
 
-    public AuthorizationMapper(UserService userService) {
+    public AuthorizationMapper(UserService userService, JwtSecret jwtSecret) {
         this.userService = userService;
+        this.jwtSecret = jwtSecret.getJtwSecret();
     }
 
     public User getUser(String token) {
-        String username = Jwts.parser().setSigningKey("SecretKeyToGenJWTs".getBytes())
+        String username = Jwts.parser().setSigningKey(jwtSecret.getBytes())
                 .parseClaimsJws(token.replace("Bearer",""))
                 .getBody()
                 .getSubject();
