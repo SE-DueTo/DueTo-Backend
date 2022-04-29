8c254c5abba03bde81459aea4d1ddc00ae649a3f
Akkjon <akkjon@outlook.de>
2022-04-29 10:39:12 +0200

    adds testcontainers

--- src/test/java/de/dueto/backend/service/SettleDebtServiceTest.java
+++ src/test/java/de/dueto/backend/service/SettleDebtServiceTest.java
@@ -7,10 +7,35 @@ import de.dueto.backend.mysql_data.SettleDebtRepository;
 import de.dueto.backend.mysql_data.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
+@ContextConfiguration(initializers = {SettleDebtServiceTest.Initializer.class})
 class SettleDebtServiceTest {
+
+    public static MySQLContainer mySQLContainer = new MySQLContainer("mysql:8.0")
+            .withDatabaseName("DueTo-Database")
+            .withUsername("DueTo")
+            .withPassword("password");
+
+    static class Initializer
+            implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+            mySQLContainer.start();
+            TestPropertyValues.of(
+                    "spring.datasource.url=" + mySQLContainer.getJdbcUrl(),
+                    "spring.datasource.username=" + mySQLContainer.getUsername(),
+                    "spring.datasource.password=" + mySQLContainer.getPassword()
+            ).applyTo(configurableApplicationContext.getEnvironment());
+        }
+    }
 
     @Autowired
     SettleDebtRepository settleDebtRepository;
@@ -21,8 +46,11 @@ class SettleDebtServiceTest {
     @Autowired
     UserRepository userRepository;
 
-    SettleDebtMapper settleDebtMapper = new SettleDebtMapper(groupRepository, userRepository);
-    SettleDebtService settleDebtService = new SettleDebtService(settleDebtRepository, settleDebtMapper);
+    @Autowired
+    SettleDebtService settleDebtService;
+
+    @Autowired
+    SettleDebtMapper settleDebtMapper;
 
     @Test
     void getBalance() {
@@ -48,7 +76,6 @@ class SettleDebtServiceTest {
 
     @Test
     void testGetDebtsUserGroupIdFromLimit() {
-
 
         assertThrows(NullPointerException.class, ()->{
             settleDebtService.getDebts(new User(), 0,1, 2);
